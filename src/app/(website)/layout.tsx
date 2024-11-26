66ba17597c004bb2280d2ed0d06e2734a6de82a7
Ben <24373331+ben-biber@users.noreply.github.com>
2024-11-26 13:48:36 +0100

    misc

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,16 +1,21 @@
-import HeaderServer from "@/blocks/global/Header/Server";
-import FooterServer from "@/blocks/global/Footer/Server";
-import React, { ReactNode } from "react";
-import '@/styles/globals.css';
+import HeaderServer from '@/blocks/global/Header/Server'
+import FooterServer from '@/blocks/global/Footer/Server'
+import React, { ReactNode } from 'react'
+import '@/styles/globals.css'
 
-export default function layout({children}: {children: ReactNode}) {
-    return (
-        <div>
-            <HeaderServer />
-            <main className="relative min-h-screen bg-white dark:bg-secondary-900">
-            {children}
-            </main>
-            <FooterServer />
-        </div>
-    );
+import type { Metadata } from 'next'
+export function generateMetadata(): Metadata {
+  return {
+    title: 'KjG Dossenheim',
+  }
+}
+
+export default function layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <HeaderServer />
+      <main className="relative min-h-screen bg-white dark:bg-secondary-900">{children}</main>
+      <FooterServer />
+    </div>
+  )
 }
