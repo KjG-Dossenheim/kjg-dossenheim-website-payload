cfe3472f3a6dbbe0f7a2d5c410fa12cf29957442
Ben <24373331+ben-biber@users.noreply.github.com>
2025-05-01 10:24:51 +0200

    Fix Navbar spacing

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -117,7 +117,7 @@ function MobileNavigation({
 function ActionsSubmenu({ aktionen }: { aktionen: ActionItem[] }) {
   return (
     <NavigationMenuContent>
-      <ul className="grid grid-flow-col grid-rows-2 p-6">
+      <ul className="grid grid-flow-col grid-rows-2 gap-2 p-6">
         <NavigationMenuItem className="row-span-2">
           <NavigationMenuLink
             className="flex h-full w-full select-none flex-col justify-center rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
