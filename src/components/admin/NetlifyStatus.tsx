4e54ae1bb6071ef65b4cf485b7a9a8e1c2c3569b
Ben <24373331+ben-biber@users.noreply.github.com>
2025-05-31 11:55:41 +0200

    Update NetlifyStatus.tsx

--- src/components/admin/NetlifyStatus.tsx
+++ src/components/admin/NetlifyStatus.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import React from 'react'
-import Image from 'next/image'
 
 export default function NetlifyStatus() {
   return (
-    <div>
-      <a href="https://app.netlify.com/sites/kjg-dossenheim-payload/deploys">
-        <Image
+    <div className="relative">
+      <a
+        href="https://app.netlify.com/sites/kjg-dossenheim-payload/deploys"
+        title="Netlify Deploy Status"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
           src="https://api.netlify.com/api/v1/badges/6c54f09b-21e6-4c9c-9ca0-1d2e6694bc51/deploy-status"
-          alt="Netlify Status"
+          alt="Netlify Deploy Status"
         />
       </a>
     </div>
