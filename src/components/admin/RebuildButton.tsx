add1a1c0ae8315e095b8eaaf20449b585d6ff678
Ben <24373331+ben-biber@users.noreply.github.com>
2025-05-31 11:55:39 +0200

    Update RebuildButton.tsx

--- src/components/admin/RebuildButton.tsx
+++ src/components/admin/RebuildButton.tsx
@@ -3,13 +3,14 @@
 'use client'
 
 import React from 'react'
+import { Button, toast } from '@payloadcms/ui'
 
 export default function RebuildButton() {
   const triggerNetlifyBuild = async () => {
     try {
       if (!process.env.NEXT_PUBLIC_REBUILD_HOOK) {
         console.error('NEXT_PUBLIC_REBUILD_HOOK environment variable is not defined.')
-        alert('Rebuild hook environment variable is not set.')
+        toast.error('Rebuild hook environment variable is not set.')
         return // Exit if the env variable is missing
       }
 
@@ -19,22 +20,20 @@ export default function RebuildButton() {
 
       if (response.ok) {
         console.log('Netlify build triggered successfully!')
-        alert('Build triggered successfully!')
+        toast.success('Build triggered successfully!')
       } else {
         console.error('Failed to trigger Netlify build:', response.statusText)
-        alert('Failed to trigger build. Check the console.')
+        toast.error('Failed to trigger build. Check the console.')
       }
     } catch (error) {
       console.error('Error triggering Netlify build:', error)
-      alert('An error occurred. Check the console.')
+      toast.error('An error occurred. Check the console.')
     }
   }
 
   return (
     <div>
-      <button className="btn btn--style-secondary btn--size-medium" onClick={triggerNetlifyBuild}>
-        Veröffentlichen
-      </button>
+      <Button onClick={triggerNetlifyBuild}>Veröffentlichen</Button>
     </div>
   )
 }
