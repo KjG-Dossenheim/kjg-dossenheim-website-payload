7a60cc5d183d2a0e93c2e0058146ae8ce597dccd
Ben <24373331+ben-biber@users.noreply.github.com>
2025-11-05 16:16:17 +0100

    Update page.tsx

--- src/app/(website)/knallbonbon/anmeldung/page.tsx
+++ src/app/(website)/knallbonbon/anmeldung/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 // React
-import React from 'react'
+import React, { Suspense } from 'react'
+
+// UI Components
+import { CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 
 // Form Component
 import { KnallbonbonAnmeldungForm } from './form'
@@ -9,7 +13,27 @@ import { KnallbonbonAnmeldungForm } from './form'
 export default function KnallbonbonAnmeldungPage() {
   return (
     <section className="mx-auto max-w-md">
-      <KnallbonbonAnmeldungForm />
+      <Suspense
+        fallback={
+          <>
+            <CardHeader>
+              <CardTitle>Knallbonbon Anmeldung</CardTitle>
+              <CardDescription>
+                Hier können Sie sich für unser Knallbonbon Event anmelden.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <Skeleton className="h-12 w-full" />
+                <Skeleton className="h-12 w-full" />
+                <Skeleton className="h-12 w-full" />
+              </div>
+            </CardContent>
+          </>
+        }
+      >
+        <KnallbonbonAnmeldungForm />
+      </Suspense>
     </section>
   )
 }
