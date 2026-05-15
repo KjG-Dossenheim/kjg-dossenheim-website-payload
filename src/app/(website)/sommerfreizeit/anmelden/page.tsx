e75a58650370167b523cfcd3b1c7fc9a483d51a0
Ben <24373331+ben-biber@users.noreply.github.com>
2026-05-15 14:12:16 +0200

    Update page.tsx

--- src/app/(website)/sommerfreizeit/anmelden/page.tsx
+++ src/app/(website)/sommerfreizeit/anmelden/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react'
+
 import { OrderLookupForm } from './OrderLookupForm'
 
 export default async function AnmeldungPage() {
   return (
     <section className="container mx-auto max-w-4xl p-6">
-      <OrderLookupForm />
+      <Suspense fallback={null}>
+        <OrderLookupForm />
+      </Suspense>
     </section>
   )
 }
