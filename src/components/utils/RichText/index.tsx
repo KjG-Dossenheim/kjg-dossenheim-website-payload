77a3db53c410db845e631949d7e591122dc86ce9
Ben <24373331+ben-biber@users.noreply.github.com>
2026-07-28 23:37:49 +0200

    Update index.tsx

--- src/components/utils/RichText/index.tsx
+++ src/components/utils/RichText/index.tsx
@@ -1,30 +1,16 @@
-import React from 'react'
 import {
   JSXConvertersFunction,
   RichText as LexicalRichText,
 } from '@payloadcms/richtext-lexical/react'
 import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
-import { DefaultNodeTypes, SerializedBlockNode } from '@payloadcms/richtext-lexical'
-import { CodeBlock, CodeBlockProps } from '@/blocks/Code/Component'
-import { FormBlock, FormBlockProps } from '@/blocks/FormBlock/Component'
-import Gallery, { GalleryBlockProps } from '@/blocks/gallery/Component'
+import { DefaultNodeTypes } from '@payloadcms/richtext-lexical'
 
-type NodeTypes =
-  | DefaultNodeTypes
-  | SerializedBlockNode<CodeBlockProps>
-  | SerializedBlockNode<FormBlockProps>
+type NodeTypes = DefaultNodeTypes
 
 const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) => ({
   ...defaultConverters,
   blocks: {
     ...defaultConverters.blocks,
-    code: ({ node }) => <CodeBlock className="col-start-2" {...node.fields} />,
-    formBlock: ({ node }: { node: SerializedBlockNode<FormBlockProps> }) => (
-      <FormBlock className="col-start-2" {...node.fields} />
-    ),
-    gallery: ({ node }: { node: SerializedBlockNode<GalleryBlockProps> }) => (
-      <Gallery {...node.fields} />
-    ),
   },
 })
 
